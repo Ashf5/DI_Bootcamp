9a96fa30f1bfc0dd5574863678715c554ad584ad
Ashf5 <friedash85@gmail.com>
2025-05-06 19:00:54 +0300

    add daily challenge week 7, made mistake

--- Week7/day4/daily-challenge/challenge1.js
+++ Week7/day4/daily-challenge/challenge1.js
@@ -1,10 +1,22 @@
 
-let sentence = 'The weather is not bad';
-let wordNot = sentence.indexOf('not');
-let wordBad = sentence.indexOf('bad');
+// let sentence = 'The weather is not bad';
+// let wordNot = sentence.indexOf('not');
+// let wordBad = sentence.indexOf('bad');
+// if (wordBad > wordNot){
+//     let sentence1 = sentence.replace(' not', '');
+//     sentence1 = sentence.replace('bad', 'good');
+// }else{
+//     console.log(sentence1);
+// }
+
+
 if (wordBad > wordNot){
-    sentence = sentence.replace('not', '');
-    sentence = sentence.replace('bad', 'good');
+    // use slicing 
+    let beg = sentence.split(0, wordNot);
+    let end = sentence.split(wordBad + 3);
+    let full = `${beg} good ${end}`;
+    console.log(end);
 }else{
     console.log(sentence);
 }
+
