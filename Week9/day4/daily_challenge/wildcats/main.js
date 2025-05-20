07b341edbce301e69f75a9bb628313c959552583
Ashf5 <friedash85@gmail.com>
2025-05-20 20:57:24 +0300

    add first daily challenge week 9 day 4

---
+++ Week9/day4/daily_challenge/wildcats/main.js
@@ -0,0 +1,39 @@
+
+const gameInfo = [
+ {
+   username: "john",
+   team: "red",
+   score: 5,
+   items: ["ball", "book", "pen"]
+ },
+ {
+   username: "becky",
+   team: "blue",
+   score: 10,
+   items: ["tape", "backpack", "pen"]
+ },
+ {
+   username: "susy",
+   team: "red",
+   score: 55,
+   items: ["ball", "eraser", "pen"]
+ },
+ {
+   username: "tyson",
+   team: "green",
+   score: 1,
+   items: ["book", "pen"]
+ },
+];
+
+const usernames = [];
+gameInfo.forEach(user => usernames.push(user.username + '!'));
+console.log(usernames);
+
+const winners = [];
+gameInfo.forEach(user => user.score > 5 ? winners.push(user.username): 0);
+console.log(winners);
+
+
+let total = gameInfo.reduce((total, user) => total + user.score, 0);
+console.log(total);
