41c0fbc50d4fd73ee0f4a27727bf5bde31e77656
Ashf5 <friedash85@gmail.com>
2025-06-03 09:37:14 +0300

    add final touches

--- Week10/day1/tic-tac-toe/main.js
+++ Week10/day1/tic-tac-toe/main.js
@@ -1,118 +1,38 @@
 
-// A class that represents the players and the positions they have on the board.
-class Player {
-    constructor(player) {
-        this.player = player;
-        this.positions = [];
+// get levels and what they're playing as
+let levelExpert;
+let opponent;
+
+document.querySelectorAll('#choice button').forEach(item => item.addEventListener('click', playerChoice))
+
+document.querySelectorAll('.levelChoice').forEach(item => item.addEventListener('click', levelChoice));
+
+function playerChoice(e) {
+    opponent = e.target.innerText;
+    document.getElementById('choice').style.display = 'none';
+
+    if (levelExpert !== undefined) {
+        board.style.display = 'unset';
+        playerTurn.innerText = `Player ${opponent}'s Turn`;
     }
 }
 
-// function returns which player who's turn it is.
-function whoTurn() {
-    if (moveCount % 2 === 0) {
-        return o;
+function levelChoice(e) {
+    if (e.target.innerText === 'Expert Level') {
+        levelExpert = true;
     } else {
-        return x;
+        levelExpert = false;
     }
-}
-
-// updates the table when a cell is pressed. Calls check winner function
-function updateTable(e) {
-    let turn = whoTurn();
-    if (e.target.innerText === '') {
-        e.target.innerText = turn.player;
-        turn.positions.push(Number(e.target.id));
-        checkWinner(turn);
-
-        // update move count to know who's turn it is.
-        moveCount++;
-        playerTurn.innerText = `Player ${turn.player}'s Turn`
+    document.getElementById('levels').style.display = 'none';
+    if (opponent !== undefined) {
+        board.style.display = 'unset';
+        playerTurn.innerText = `Player ${opponent}'s Turn`;
     }
-    // remove from the available options
-    available = available.filter(num1 => num1 !== turn.positions[turn.positions.length - 1]);
 
-    // For now just the simple version
-    if (true && available.length > 0) {
-        makeRandomMove();
-    }
-}
-
-// function deals with the board if there is a winner
-function dealWinner(winner = 'tie', index = 'tie') {
-    console.log('called dealwinner!');
-
-    if (winner !== 'tie') {
-        table.style.background = 'green';
-        table.removeEventListener('click', updateTable);
-        for (i of winCombos[index]) {
-            data[i].style.background = 'red';
-
-        }
-    } else {
-        table.style.background = 'green';
-        table.removeEventListener('click', updateTable);
-
-    }
 
 }
 
-// checks if there is a winner and returns which player won or blank if tie.
-function checkWinner(turn) {
-    let i = 0
-    let contains = winCombos.some((item, index) => {
-        let hasAll = item.every(item2 => turn.positions.includes(item2));
-        i = index;
-        return hasAll;
-    });
-
-
-    if (contains == true) {
-        dealWinner(turn, index = i);
-    } else if (Array.from(data).every(item => !(item.innerText === ''))) {
-        dealWinner(index = i);
-    }
-
-}
-
-
-function makeRandomMove() {
-    let turn = whoTurn();
-    let num = Math.floor(Math.random() * available.length);
-    // get the cell 
-    let cell = Array.from(data).filter(item => item.id.endsWith(available[num]))[0];
-    cell.innerText = turn.player;
-
-    // add to positions 
-    turn.positions.push(available[num]);
-    checkWinner(turn);
-
-    moveCount++;
-    playerTurn.innerText = `Player ${whoTurn().player}'s Turn`;
-
-
-
-    // remove from the available options
-    available = available.filter(num1 => num1 !== available[num]);
-}
-
-
-let moveCount = 0;
-let x = new Player('X');
-let o = new Player('O');
-
-const winCombos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [6, 4, 2]
-]
-
-let available = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-
+let board = document.getElementsByClassName('board')[0];
 let table = document.querySelector('table');
 let data = document.getElementsByTagName('td');
 let playerTurn = document.getElementById('player-turn');
@@ -121,3 +41,284 @@ let playerTurn = document.getElementById('player-turn');
 table.addEventListener('click', updateTable);
 
 
+// functions deal with table 
+
+// update the table on click
+function updateTable(e) {
+    let nums = convertId(e.target.id);
+
+    let open = options(boardInternal).some(arr => {
+        let all = arr.every((element, index) => nums[index] == element
+        )
+        return all
+    });
+    if (open == true) {
+        let player = getPlayer(boardInternal);
+        e.target.innerText = player;
+        boardInternal[nums[0]][nums[1]] = player;
+
+        // Call the computer generated move
+        if (levelExpert) {
+            minimax(boardInternal);
+        } else {
+            makeRandomMove();
+        }
+    }
+
+
+}
+
+// function to deal with winner 
+
+function dealWinner(coordinates) {
+    table.style.background = 'green';
+    table.removeEventListener('click', updateTable);
+    if (coordinates !== 'tie') {
+        for (element of coordinates) {
+            let id = convertId(element, reverse = true);
+            document.getElementById(`${id}`).style.background = 'red';
+        }
+    }
+}
+
+
+// takes an index and converts into indexes for nested array.
+function convertId(num, reverse = false) {
+    if (reverse === true) {
+        let total = num[0] * 3;
+        total += num[1];
+        return total;
+    }
+    return [Math.floor(num / 3), num % 3]
+}
+
+
+
+// Deal with the lower level random moves
+function makeRandomMove() {
+    let turn = getPlayer(boardInternal);
+    let open = options(boardInternal);
+
+    let num = Math.floor(Math.random() * open.length);
+
+    // get the cell and if tie call deal winner.
+    try {
+        let cell = convertId(open[num], reverse = true);
+        document.getElementById(cell).innerText = turn;
+        boardInternal[open[num][0]][open[num][1]] = turn;
+    } catch (err) {
+        dealWinner('tie');
+    }
+
+
+
+    // check winner 
+    let winner = isFinal(boardInternal);
+    if (winner !== null) {
+        dealWinner(winner);
+
+    }
+
+
+    playerTurn.innerText = `Player ${opponent}'s Turn`;
+
+}
+
+
+
+
+
+// minimax functions and stuff
+function makeMiniMove(cellArray) {
+    let turn = getPlayer(boardInternal);
+    // check if tie.
+    try {
+        let cell = convertId(cellArray, reverse = true);
+
+        document.getElementById(`${cell}`).innerText = turn;
+        boardInternal[cellArray[0]][cellArray[1]] = turn;
+    } catch (err) {
+        dealWinner('tie');
+    }
+
+
+    let winner = isFinal(boardInternal);
+    if (winner !== null) {
+        dealWinner(winner);
+
+    }
+
+    playerTurn.innerText = `Player ${opponent}'s Turn`;
+
+}
+
+function minimax(board) {
+    let player = opponent === 'X' ? 'O' : 'X';
+    playerTurn.innerText = `Player ${player}'s Turn`;
+    // Get all moves 
+    let possible = options(board);
+
+    let best;
+    if (player === 'O') {
+        best = [-1, -1000];
+        for (move in possible) {
+            let util = min(createState(board, possible[move]));
+
+            if (util === 1) {
+                makeMiniMove(possible[move]);
+                return
+            } else if (util > best[1]) {
+                best[0] = move;
+                best[1] = util;
+            }
+        }
+
+    } else {
+        best = [-1, 1000];
+        for (move in possible) {
+            let util = max(createState(board, possible[move]));
+            if (util === -1) {
+                makeMiniMove(possible[move]);
+                return
+            } else if (util < best[1]) {
+                best[0] = move;
+                best[1] = util;
+            }
+        }
+
+    }
+
+    // get the best option 
+
+    makeMiniMove(possible[best[0]])
+}
+
+function min(board) {
+    let boardCopy = structuredClone(board)
+    let util = utility(boardCopy);
+
+
+    if (util !== null) {
+        return util;
+    }
+
+    let minimum = Infinity;
+    // get all options 
+    let availableMoves = options(boardCopy);
+    for (let move of availableMoves) {
+        let utilNew = max(createState(boardCopy, move))
+        if (utilNew < minimum) {
+            minimum = utilNew;
+        }
+    }
+    return minimum
+
+
+
+
+}
+
+function max(board) {
+    let boardCopy = structuredClone(board)
+
+
+    let util = utility(boardCopy);
+
+    if (util !== null) {
+        return util;
+    }
+
+    let maximum = -Infinity;
+    // get all options 
+    let availableMoves = options(boardCopy);
+    for (let move of availableMoves) {
+        let utilNew = min(createState(boardCopy, move))
+        if (utilNew > maximum) {
+            maximum = utilNew;
+        }
+    }
+    return maximum;
+
+}
+
+// function to create how board would look
+function createState(board, index) {
+    // make sure it's a valid move
+    let boardCopy = structuredClone(board);
+    if (boardCopy[index[0]][index[1]] !== '') {
+        return null;
+    } else {
+        boardCopy[index[0]][index[1]] = getPlayer(board);
+        return boardCopy
+    }
+}
+
+// Takes in a board and returns who's turn it is
+function getPlayer(board) {
+    // See how many squares are blank
+    let count = 0;
+    for (let row of board) {
+        row.forEach(item => item !== '' ? count++ : null);
+    }
+    if (count % 2 === 0) {
+        return opponent;
+    } else {
+        return opponent === 'X' ? 'O' : 'X'
+    }
+}
+
+function isFinal(board) {
+    for (let i = 0; i < 3; i++) {
+        if (board[i][0] !== '' && board[i][0] === board[i][1] && board[i][1] == board[i][2]) {
+            return [[i, 0], [i, 1], [i, 2]];
+        } else if (board[0][i] !== '' && board[0][i] === board[1][i] && board[1][i] == board[2][i]) {
+            return [[0, i], [1, i], [2, i]];
+        }
+    }
+    // Check diagnols
+    if (board[0][0] !== '' && board[0][0] === board[1][1] & board[1][1] === board[2][2]) {
+        return [[0, 0], [1, 1], [2, 2]]
+    } else if (board[0][2] !== '' && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+        return [[0, 2], [1, 1], [2, 0]];
+    }
+    // check if tie
+    if (!(board.some(item => item.includes('')))) {
+        return 'tie';
+    }
+    return null;
+}
+
+// takes a final board and returns the utility
+function utility(board) {
+    // let player = getPlayer(board);
+    let player = opponent === 'X' ? 'O' : 'X';
+    let score = isFinal(board);
+    if (score === null) {
+        return null;
+    } else if (score === 'tie') {
+        return 0;
+    } else if (board[score[0][0]][score[0][1]] === 'O') {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+// returns all options
+function options(board) {
+    let available = []
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (board[i][j] === '') {
+                available.push([i, j]);
+            }
+        }
+    }
+    return available;
+}
+
+// Internal representation 
+let boardInternal =
+    [['', '', ''],
+    ['', '', ''],
+    ['', '', '']];
